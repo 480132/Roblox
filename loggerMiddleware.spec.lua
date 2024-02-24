a9b2fd79e904c7590f380a764434506f5aaff40a
480132 <83782172+480132@users.noreply.github.com>
2024-02-24 14:27:03 -0800

    Update loggerMiddleware.spec.lua

--- loggerMiddleware.spec.lua
+++ loggerMiddleware.spec.lua
@@ -10,6 +10,26 @@ return function()
 			return state
 		end
 
+	for (int y = minExt.y; y <= maxExt.y; ++y) {
+		for (int z = minExt.z; z <= maxExt.z; ++z) {
+			for (int x = minExt.x; x <= maxExt.x; ++x) {
+				if (x < minX || x > minX + 1 || z < minZ || z > minZ + 1 || y < minY || y > minY) {
+					BOOST_CHECK_EQUAL(CELL_MATERIAL_Deprecated_Empty,
+						fixture.cluster->getCellScript(x, y, z)->at(0).cast<CellMaterial>());
+				} else {
+					int index = (x - minX) + 2 * (z - minZ);
+					BOOST_REQUIRE_LE(index, 3);
+					BOOST_REQUIRE_GE(index, 0);
+					Cell expected = expectedCells[index];
+					Cell actual = fixture.cluster->getVoxelGrid()->getCell(Vector3int16(x + kOffset, y, z + kOffset));
+					char messageBuffer[256];
+					snprintf(messageBuffer, 256, "(%d,%d,%d) %d != %d", x, y, z,
+						Cell::asUnsignedCharForDeprecatedUses(expected),
+						Cell::asUnsignedCharForDeprecatedUses(actual));
+					BOOST_CHECK_MESSAGE(expected == actual, messageBuffer);
+				}
+			}
+		}
 		local store = Store.new(reducer, {
 			fooValue = 12345,
 			barValue = {
